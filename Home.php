056ede182ef3342499bcea749cfed416a1305f05
jaafar01 <105442665+jaafar01@users.noreply.github.com>
2023-05-03 19:01:45 +0100

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,161 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./style.css" />
+    <title>Sport UCA</title>
+  </head>
+  <body>
+    <div class="container">
+      <header class="header">
+        <img src="logo.png" alt="logo-uca" class="logoNavBar" />
+        <div class="headerBtnGroup">
+          <button class="navBtn">Se connecter</button>
+          <button class="navBtn">Admin</button>
+          <button class="navBtn">A propos</button>
+        </div>
+      </header>
+      <marquee><p class="sectionTittle" id="backToTop">uca</p></>
+      <p class="sectionDescription">
+        Aqui é onde você encontra todos os itens mais novos e modernos do seu
+        esporte preferido
+      </p>
+      <section class="gridContainer">
+        <div class="mainContent">
+          <div class="categoryCard">
+            <img src="./boxe.jpg" alt="mainCardImg" class="mainCardImg" />
+            <p class="mainCategoryCardTittle">BOXE</p>
+            <p class="mainCategoryCardDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              voluptatibus provident repellat nemo sit cum ducimus voluptatum
+              modi iste porro.
+            </p>
+          </div>
+          <div class="categoryCard">
+            <img src="./crossfit.jpg" alt="mainCardImg" class="mainCardImg" />
+            <p class="mainCategoryCardTittle">CROSSFIT</p>
+            <p class="mainCategoryCardDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              voluptatibus provident repellat nemo sit cum ducimus voluptatum
+              modi iste porro.
+            </p>
+          </div>
+          <div class="categoryCard">
+            <img src="./natacao.jpg" alt="mainCardImg" class="mainCardImg" />
+            <p class="mainCategoryCardTittle">NATAÇÃO</p>
+            <p class="mainCategoryCardDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              voluptatibus provident repellat nemo sit cum ducimus voluptatum
+              modi iste porro.
+            </p>
+          </div>
+          <div class="categoryCard">
+            <img src="./roupas.jpg" alt="mainCardImg" class="mainCardImg" />
+            <p class="mainCategoryCardTittle">ROUPAS</p>
+            <p class="mainCategoryCardDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              voluptatibus provident repellat nemo sit cum ducimus voluptatum
+              modi iste porro.
+            </p>
+          </div>
+          <div class="categoryCard">
+            <img src="./basquete.jpg" alt="mainCardImg" class="mainCardImg" />
+            <p class="mainCategoryCardTittle">BASQUETE</p>
+            <p class="mainCategoryCardDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              voluptatibus provident repellat nemo sit cum ducimus voluptatum
+              modi iste porro.
+            </p>
+          </div>
+          <div class="categoryCard">
+            <img src="./corrida.jpg" alt="mainCardImg" class="mainCardImg" />
+            <p class="mainCategoryCardTittle">CORRIDA</p>
+            <p class="mainCategoryCardDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              voluptatibus provident repellat nemo sit cum ducimus voluptatum
+              modi iste porro.
+            </p>
+          </div>
+          <div class="categoryCard">
+            <img src="./surf.jpg" alt="mainCardImg" class="mainCardImg" />
+            <p class="mainCategoryCardTittle">SURF</p>
+            <p class="mainCategoryCardDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              voluptatibus provident repellat nemo sit cum ducimus voluptatum
+              modi iste porro.
+            </p>
+          </div>
+          <div class="categoryCard">
+            <img src="./trilha.jpg" alt="mainCardImg" class="mainCardImg" />
+            <p class="mainCategoryCardTittle">TRILHA</p>
+            <p class="mainCategoryCardDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              voluptatibus provident repellat nemo sit cum ducimus voluptatum
+              modi iste porro.
+            </p>
+          </div>
+          <div class="categoryCard">
+            <img src="./tenis.jpg" alt="mainCardImg" class="mainCardImg" />
+            <p class="mainCategoryCardTittle">TÊNIS</p>
+            <p class="mainCategoryCardDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+              voluptatibus provident repellat nemo sit cum ducimus voluptatum
+              modi iste porro.
+            </p>
+          </div>
+        </div>
+        <div class="sidebarContent">
+          <p>CATEGORIAS EXTRAS</p>
+          <div class="sidebarCard">
+            <img
+              src="./bicicletas.jpg"
+              alt="sidebarCardImg"
+              class="sidebarCardImg"
+            />
+            <p class="sidebarCardTittle">BICICLETAS</p>
+          </div>
+          <div class="sidebarCard">
+            <img
+              src="./esportesNaNeve.jpg"
+              alt="sidebarCardImg"
+              class="sidebarCardImg"
+            />
+            <p class="sidebarCardTittle">ESPORTES DE FRIO</p>
+          </div>
+          <div class="sidebarCard">
+            <img
+              src="./futebolAmericano.jpg"
+              alt="sidebarCardImg"
+              class="sidebarCardImg"
+            />
+            <p class="sidebarCardTittle">FUTEBOL AMERICANO</p>
+          </div>
+          <div class="sidebarCard">
+            <img
+              src="./skate.jpg"
+              alt="sidebarCardImg"
+              class="sidebarCardImg"
+            />
+            <p class="sidebarCardTittle">SKATE</p>
+          </div>
+          <div class="sidebarCard">
+            <img src="./yoga.jpg" alt="sidebarCardImg" class="sidebarCardImg" />
+            <p class="sidebarCardTittle">YOGA</p>
+          </div>
+        </div>
+      </section>
+      <footer class="footer">
+        <img
+          src="./logo-onebitcode.svg"
+          alt="logo-onebitcode"
+          class="logoFooter"
+        />
+        <a href="#backToTop" class="footerAnchor">VOLTAR PARA O TOPO</a>
+      </footer>
+    </div>
+  </body>
+</html>
