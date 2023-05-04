40941995c45a51c8be50f96c686741b1ab8ebb1f
jaafar01 <105442665+jaafar01@users.noreply.github.com>
2023-05-04 01:51:07 +0100

    Update login.php

--- login.php
+++ login.php
@@ -5,16 +5,14 @@
       <title>Sign Up</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta charset="utf-8" />
-      <link rel="stylesheet" type="text/css" href="signup_style.css" />
+      <link rel="stylesheet" type="text/css" href="login-style.css" />
       
      
       <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
     </head>
-
     <body class="body">
      
-
       <div class="login-page">
         <div class="form">
           <form>
@@ -28,18 +26,34 @@
             ></lottie-player>
             <div class="select">
               <select name="format" id="format">
-                 <option selected disabled>Choose a book format</option>
-                 <option value="pdf">Admin</option>
-                 <option value="txt">SuperAdmin</option>
+                 <option selected disabled>Choisir votre établisement</option>
+                 <option value="super admin">Admin</option>
+                 <option value="FST">FST</option>
+                 <option value="FSTG">FSTG</option>
+                 <option value="ENSAM">ENSA-MARRAKECH</option>
+                 <option value="FSSM">FSSM</option>
+                 <option value="FMPM">FMPM</option>
+                 <option value="FLSH">FLSH</option>
+                 <option value="FSJESM">FSJESM</option>
+                 <option value="SLAM">SLAM</option>
+                 <option value="ENS">ENS</option>
+                 <option value="ENCG">ENCG</option>
+                 <option value="EST-ESSAOUIRA">EST-ESSAOUIRA</option>
+                 <option value="EST-SAFI">EST-SAFI</option>
+                 <option value="EST-KELAA">EST-KELAA</option>
+                 <option value="ENSA-SAFI">ENSA-SAFI</option>
+                 <option value="F.POLY-SAFI">F.POLY-SAFI</option>
+                 <option value="FSJES-KALAA">ESJES-KALAA</option>
+                 <option value="CUMCES">CUMCES</option>
+                 <option value="HEEC">HEEC</option>
               </select>
            </div>
-            <input type="text" placeholder="Username" />
-            <input type="password" id="password" placeholder="mot de passe" />
+            <input type="password" id="password" placeholder=" password" />
+            <br>
             <i class="fas fa-eye" onclick="show()"></i>
             <br>
             <br>
           </form>
-
           <form class="login-form">
             <button type="button" onclick="window.location.href='login.html'">
               Se connecter
@@ -52,7 +66,6 @@
       function show() {
         var password = document.getElementById("password");
         var icon = document.querySelector(".fas");
-
         // ========== Checking type of password ===========
         if (password.type === "password") {
           password.type = "text";
